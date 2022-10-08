0b99081679f0771c7786c7f8a0ff6bb8502db387
Claudbest1 <claudiusluv50@gmail.com>
2022-10-08 14:00:03 +0100

    update

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,7 +3,55 @@ import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section class="about--content--container">
+                        
+                        <article class="about--content">
+                            <h1 class="about--content--title">Our Mission</h1>
+                            <p class="about--content--text">
+                                Our mission is to offer a platform to users that
+                                work with
+                                enormous data files, such as data analysts, data
+                                scientists,
+                                developers, and others, where they can split
+                                those files into a
+                                certain number of sections to make their files
+                                easier to work
+                                with.
+                            </p>
+                            <p class="about--content--text">
+                                Go-Chunk provides guaranteed security by
+                                protecting our users
+                                privacy. All actions directly related to the
+                                project are covered
+                                by the
+                                <a class="intext--links"
+                                    href="./privacy_new.html">Go-Chunk
+                                    privacy
+                                    policy.</a>
+                                This is to ensure users can freely perform tasks
+                                on the project
+                                with safety and assured security. Check out our
+                                <a class="intext--links" href="./t&c_new.html">Terms
+                                    and Conditions</a>
+                                to know our legal propositions.
+                            </p>
+                            <p class="about--content--text">
+                                We also provide our users with the options to
+                                download or save
+                                their files in their desired formats. Users can
+                                always come back
+                                to download their files within a certain time
+                                frame. Click on our
+                                <a class="intext--links" href="./t&c_new.html">terms
+                                    and Conditions</a>,
+                                for more details.
+                            </p>
+
+                            <button class="about--contact--form--btn">Send
+                                message</button>
+                        </article>
+                    </section>
+                
   )
 }
 
